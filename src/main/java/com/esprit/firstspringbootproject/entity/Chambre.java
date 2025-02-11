1e35fef5488c8bb95e2b35b80cdd87ba923c84d6
Ameni-boukottaya14 <ameni.boukattaya@esprit.tngit config --global user.email ameni.boukattaya@esprit.tn>
2025-02-11 12:06:27 +0100

    first commit

---
+++ src/main/java/com/esprit/firstspringbootproject/entity/Chambre.java
@@ -0,0 +1,24 @@
+package com.esprit.firstspringbootproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chambre  implements Serializable {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int idChambre;
+        private int numeroChambre;
+        @Enumerated(EnumType.STRING)
+        private TypeChambre TypeC;
+}
---
+++ src/main/java/com/esprit/firstspringbootproject/entity/TypeChambre.java
@@ -0,0 +1,7 @@
+package com.esprit.firstspringbootproject.entity;
+
+public enum TypeChambre {
+    SIMPLE,
+    DOUBLE,
+    TRIPLE
+}
